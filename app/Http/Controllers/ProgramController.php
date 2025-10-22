d0745d0164da417c9c2186f4119664b7294bd5e3
suliman-btk <suliman56563@gmail.com>
2025-10-22 19:29:57 +0800

    ADD: include Log facade in ProgramController for enhanced logging capabilities.

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -6,6 +6,7 @@ use App\Models\Program;
 use App\Enums\UserRole;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
