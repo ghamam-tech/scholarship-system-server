138e7c6f1e03b817524ee2971d870831f03182a5
alassaf-abdulrahman <abodoassaf112@gmail.com>
2025-10-22 10:51:30 +0800

    FIX: Fixd the CORS issue

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['http://localhost:3000/*', 'http://127.0.0.1:3000/*'],
+    'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
 
     'allowed_origins_patterns' => [],
 
